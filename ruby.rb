7aed57b96a823ee8a9abeb6b8c0f02ec4129c7d5
Ubuntu <ubuntu@ip-172-31-38-73.us-east-2.compute.internal>
2019-07-11 21:54:42 +0000

    here is my finished ruby assessment, sorry for the delay!

--- ruby.rb
+++ ruby.rb
@@ -1,50 +1,96 @@
-# Ruby Assessments
+# # Ruby Assessments
 
-#### 1. Use two different Ruby loops to loop over this array, multiplying each element by 2.
+# #### 1. Use two different Ruby loops to loop over this array, multiplying each element by 2.
+puts this sentence
+# tempArray = [1, 2, 6, 9, 3, 21]
 
-tempArray = [1, 2, 6, 9, 3, 21]
+# # your ruby loop here
+p tempArray.map { |num| num * 2 }
+tempArray.each do |num|
+    num = num * 2
+    puts num
+end
 
-# your ruby loop here
 
-
-#### 2. From all the built in Ruby methods we've seen in class this week, choose three that you think are particularly helpful and create examples to show how they work.
+# #### 2. From all the built in Ruby methods we've seen in class this week, choose three that you think are particularly helpful and create examples to show how they work.
 
 # your three built in ruby methods
 
+mantra = "charlie is the Best"
 
-#### 3. Create a method that takes in a sentence and returns a new sentence with the first letter of each word capitalized.
+
+p mantra.capitalize
+
+p mantra.upcase
+
+p mantra.downcase
+
+
+# #### 3. Create a method that takes in a sentence and returns a new sentence with the first letter of each word capitalized.
 
 sentence = "hello there, how are you?"
-expected output = "Hello There, How Are You?"
 
-#### 4.  Create a method that takes in a string and returns a new string with all the vowels removed. HINT:  there's a built in string method for this
+def cap_first(array)
+    puts array.split.map(&:capitalize).join(' ')
+end
+
+cap_first(sentence)
+
+# #### 4.  Create a method that takes in a string and returns a new string with all the vowels removed. HINT:  there's a built in string method for this
 
 no_vowels = "I have no vowels"
-expected output = " hv n vwls"
 
+def remove_vowels(string)
+    p string.delete('aeiou')
+end
 
-#### 5. Look at this horrible ruby code, and fix it to be good ruby code.
+remove_vowels(no_vowels)
 
-``` ruby
-class example
+# #### 5. Look at this horrible ruby code, and fix it to be good ruby code.
+
+class Example
   def initialize(day)
-    @day=day
+    @day = day
   end
-
-  def Say_hi
-    if(day == "Friday"){
+  def say_hi
+    if @day == "Friday"
       puts "TGIF!"
-    }
-    elseif(day == "Monday"){
+    elsif @day == "Monday"
       puts "Its monday again"
-    }
     else
       puts "another day"
+    end
   end
 end
-```
-#### 6a.  Create a class called Animal that initializes with a color.  Create a method in the class called legs that returns 4.
 
-# your class here
+day1 = Example.new("Monday")
+day2= Example.new("Tuesday")
+day5= Example.new("Friday")
 
-#### 6b.  Create a new instance of an Animal with a brown color.  Return how many legs the animal has.
+day1.say_hi
+day2.say_hi
+day5.say_hi
+
+
+# #### 6a.  Create a class called Animal that initializes with a color.  Create a method in the class called legs that returns 4.
+
+class Animal
+  def initialize(color)
+    @color = color
+  end
+  
+  def color
+    @color
+  end
+  
+  def legs
+    puts 4
+  end
+end
+
+
+# #### 6b.  Create a new instance of an Animal with a brown color.  Return how many legs the animal has.
+
+animal_example = Animal.new("Brown")
+
+animal_example.legs
